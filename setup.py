8ee3ecf9b562b02424f313fb883f32440f3dc558
CoolRobotsAndStuff <aledeum.saf@gmail.com>
2022-04-27 11:36:37 -0300

    changed repo if setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-setup(name='keras-rl2',
+setup(name='keras-rl2-mlflow',
       version='1.0.5',
       description='Deep Reinforcement Learning for Tensorflow 2 Keras',
       author='Taylor McNally',
-      url='https://github.com/wau/keras-rl2',
+      url='https://github.com/CoolRobotsAndStuff/keras-rl2-mlflow',
       license='MIT',
       install_requires=['tensorflow'],
       extras_require={
